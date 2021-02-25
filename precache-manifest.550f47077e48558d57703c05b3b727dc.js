882d6bcfc61af6336c5339790aa391ad06d7099b
daototpyrc <daototpyrc@gmail.com>
2021-02-24 16:27:59 -0800

    update build for latest drop

---
+++ precache-manifest.550f47077e48558d57703c05b3b727dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f2a985de7dff944817a29d696b682a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "425e8a7e86fa440a27c2",
+    "url": "/static/css/main.56af647e.chunk.css"
+  },
+  {
+    "revision": "12a772494bf8f8becef1",
+    "url": "/static/js/2.33ecce70.chunk.js"
+  },
+  {
+    "revision": "1ec54c518ced73552a48c25815983d38",
+    "url": "/static/js/2.33ecce70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "425e8a7e86fa440a27c2",
+    "url": "/static/js/main.ee9b1f9f.chunk.js"
+  },
+  {
+    "revision": "145efac542755355e33c",
+    "url": "/static/js/runtime-main.e0770c88.js"
+  },
+  {
+    "revision": "2db009d884b526dbd3ccd985cb52de25",
+    "url": "/static/media/regular.2db009d8.ttf"
+  }
+]);
