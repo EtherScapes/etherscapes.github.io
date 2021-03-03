2d1b232d52904837fc1b9fbd16e8ad67437d1edb
daototpyrc <daototpyrc@gmail.com>
2021-03-03 14:31:05 -0800

    updated site with hash router and new rinkeby contracts

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render((
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+  <HashRouter>
+    <App />
+  </HashRouter>
 ), document.getElementById("root"));
 
 registerServiceWorker();
